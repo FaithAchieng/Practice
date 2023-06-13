822bc74cdf297f272e15b3b1bfe2f58ee52ec637
FaithAchieng <135111683+FaithAchieng@users.noreply.github.com>
2023-06-13 13:48:12 +0300

    Add files via upload
    Ubuntu commands

---
+++ Ubuntu_commands.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+sudo
+
+#!/bin/bash
+apt-get commands
+
+#!/bin/bash
+apt-get 
+
+#!/bin/bash
+apt-get upgrade
+
+#!/bin/bash
+apt-get disk upgrade
+
+#!/bin/bash
+apt-get install
+
+#!/bin/bash
+apt-get remove
+
+#!/bin/bash
+apt-get purge
+
+#!/bin/bash
+apt-get clean
+
+#!/bin/bash
+apt-get autoclean
+
+#!/bin/bash
+apt-get autoremove
+
+#!/bin/bash
+date
+
+#!/bin/bash
+df
+
+#!/bin/bash
+du
+
+#!/bin/bash
+free
+
+#!/bin/bash
+uname
+
+#!/bin/bash
+top
+
+#!/bin/bash
+ps
+
+#!/bin/bash
+uptime
+
+#!/bin/bash
+ls
+
+#!/bin/bash
+cp
+
+#!/bin/bash
+mv
+
+#!/bin/bash
+rm
+
+#!/bin/bash
+rmdir 
+
+#!/bin/bash
+less
+
+#!/bin/bash
+history
+
+#!/bin/bash
+man
+
+#!/bin/bash
+info
+
+#!/bin/bash
+passwd
+
+#!/bin/bash
+whatis
+
+#!/bin/bash
+finger
+
+#!/bin/bash
+cat
